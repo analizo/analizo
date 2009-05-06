86dd2fc66672c0560c11df42e983945e7bed56ef
Joenio Costa <joenio@perl.org.br>
2009-05-06 11:50:36 -0300

    parsing more then input with doxyparse and extract variable use

--- Egypt/Extractor/Doxyparse.pm
+++ Egypt/Extractor/Doxyparse.pm
@@ -23,6 +23,10 @@ sub feed {
     $self->model->declare_function($self->current_module, $1);
     $self->{current_function} = $1;
   }
+  # variable declarations
+  elsif ($line =~ m/^\s{3}variable (\S+) in line \d+$/) {
+    $self->model->declare_variable($self->current_module, $1);
+  }
 
   # function calls/uses
   if ($line =~ m/^\s{6}uses function (\S+) defined in (\S+)$/) {
@@ -30,6 +34,7 @@ sub feed {
     $self->model->add_call($self->current_function, $1, 'direct');
   #} elsif ($line =~ m/^.*\(symbol_ref.*"(.*)".*<function_decl\s.*$/) {
     # indirect calls (e.g. use of function pointers)
+    # indirect calls currently unimplemented by doxyparse
     #$self->model->add_call($self->current_function, $1, 'indirect');
   }
 
@@ -37,31 +42,27 @@ sub feed {
   if ($line =~ m/^\s{6}uses variable (\S+) defined in (\S+)$/) {
     $self->model->add_variable_use($self->current_function, $1);
   }
-
 }
 
 sub process {
   my $self = shift;
   my @files = ();
-  foreach my $arg (@_) {
-    $self->info("Parsing input $arg with Doxyparse ...");
-    eval {
-      open DOXYPARSE, sprintf("doxyparse %s |", $arg) or die $!;
-      while (<DOXYPARSE>) {
-         if (/^module (\S+)$/) {
-           # discard input file suffix
-           my $modulename = fileparse($1, qr/\.[^.]*/);
-           $self->current_module($modulename);
-         }
-         else {
-           $self->feed($_);
-         }
-      }
-      close DOXYPARSE;
-    };
-    if($@) {
-      exit -1;
+  $self->info("Parsing input '" . join(', ', @_) . "' with Doxyparse ...");
+  eval {
+    open DOXYPARSE, sprintf("doxyparse %s |", join(' ', @_) ) or die $!;
+    while (<DOXYPARSE>) {
+       if (/^module (\S+)$/) {
+         my $modulename = fileparse($1, qr/\.[^.]*/); # discard file suffix (e.g. .c or .h)
+         $self->current_module($modulename);
+       }
+       else {
+         $self->feed($_);
+       }
     }
+    close DOXYPARSE;
+  };
+  if($@) {
+    exit -1;
   }
 }
 
