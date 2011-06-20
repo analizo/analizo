1076cef9b945011b4f3a530adce514bc67d596df
Antonio Terceiro <terceiro@softwarelivre.org>
2011-06-20 14:59:08 -0700

    Support testing content of non-numeric fields

--- features/step_definitions/analizo_steps.rb
+++ features/step_definitions/analizo_steps.rb
@@ -109,10 +109,13 @@ Then /^analizo must report that the project has (.+) = ([\d\.]+)$/ do |metric,n|
   stream.documents.first[metric].should == n.to_f
 end
 
-Then /^analizo must report that module (.+) has (.+) = (\d+|\d+\.\d+)$/ do |mod, metric, n|
+Then /^analizo must report that module (.+) has (.+) = (.+)$/ do |mod, metric, value|
   stream = YAML.load_stream(@stdout.join)
   module_metrics = stream.documents.find { |doc| doc['_module'] == mod }
-  module_metrics[metric].should == n.to_f
+  if value =~ /^\d+|\d+\.\d+$/
+    value = value.to_f
+  end
+  module_metrics[metric].should == value
 end
 
 Then /^analizo must present a list of metrics$/ do
