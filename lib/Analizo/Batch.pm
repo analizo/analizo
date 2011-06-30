bd04c32335b1412d4bc4e48216f344d916942bc5
Antonio Terceiro <terceiro@softwarelivre.org>
2011-06-29 20:07:00 -0700

    Rephrase explanation in comment

--- lib/Analizo/Batch.pm
+++ lib/Analizo/Batch.pm
@@ -10,9 +10,9 @@ sub new {
 
 # This method must be overriden by subclasses, and must return a object that
 # inherits from Analizo::Job representing the next job to be processed in this
-# batch. If there are no pending jobs, i.e. all jobs contained by this job were
-# already returns, then this method must return I<undef> to signal the end of
-# the batch.
+# batch. If there are no pending jobs, i.e. all jobs contained in this batch
+# were already returned, then this method must return I<undef> to signal the
+# end of the batch.
 sub next {
   return undef;
 }
