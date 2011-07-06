238159ba2b22ca4d3123d4f0e639544bdc9a4004
Antonio Terceiro <terceiro@softwarelivre.org>
2011-07-06 15:40:07 -0700

    Remove uneeded complexity from base Runner class

--- lib/Analizo/Batch/Runner.pm
+++ lib/Analizo/Batch/Runner.pm
@@ -7,15 +7,15 @@ sub new {
   return bless {}, $class;
 }
 
-__PACKAGE__->mk_accessors(qw(output));
-
 # must be implemented by subclasses. Will receive as argument:
+#
 #   * the batch to be run
 #   * the output object
 #
 # This method must iterate over the jobs in some way, depending on the strategy
-# employed by the subclass, and call I<execute> on each job, and feed the job
-# object to the $output object by calling I<$output->push($job)>.
+# employed by the subclass, call I<execute> on each job, and feed the job
+# object to the $output object by calling I<$output->push($job)>. In the end,
+# the runner must call flush() on the output object.
 sub run {
 }
 
