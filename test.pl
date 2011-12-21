54678ea096f794b56d79319106e84e638965c7f4
Antonio Terceiro <terceiro@softwarelivre.org>
2011-12-21 14:57:41 -0200

    Print ruby and cucumber versions before tests

--- test.pl
+++ test.pl
@@ -1,5 +1,7 @@
 if (system('which cucumber >/dev/null') == 0) {
   print "I: Running acceptance tests with cucumber ...\n";
+  system('ruby --version');
+  system('echo -n "cucumber " ;cucumber --version');
   system('cucumber --format progress --tags ~@wip features/') == 0 or exit(1);
 } else {
   print "W: cucumber not found, not running acceptance tests.\n";
