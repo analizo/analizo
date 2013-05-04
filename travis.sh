8a41c77c9c495c3db52fab4ac87dd76b1361925b
Antonio Terceiro <terceiro@softwarelivre.org>
2013-05-04 17:51:54 -0300

    I think I figured out what's up with libzmq on travis

--- travis.sh
+++ travis.sh
@@ -1,10 +1,19 @@
 # doxyparse
-echo 'deb http://analizo.org/download/ ./' > /etc/apt/sources.list.d/analizo.list
-wget -O - http://analizo.org/download/signing-key.asc | apt-key add -
-apt-get update
-apt-get install doxyparse
-rm -rf /etc/apt/sources.list.d/analizo.list
-apt-get update
+echo 'deb http://analizo.org/download/ ./' | sudo tee /etc/apt/sources.list.d/analizo.list
+wget -O - http://analizo.org/download/signing-key.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install doxyparse
+sudo rm -rf /etc/apt/sources.list.d/analizo.list
+sudo apt-get update
 
 # rest of non-perl deps
-apt-get install git sloccount sqlite3 man pkg-config uuid-dev rake cucumber ruby-rspec
+sudo apt-get install git sloccount sqlite3 man pkg-config uuid-dev rake cucumber ruby-rspec
+
+# libraries needed to build ZeroMQ. Note libzmq3-dev comes from the zeromq PPA
+# and not from official repos - you should not use that package name anywhere else
+sudo apt-get install uuid-dev libzmq3-dev
+
+if ! cpanm --installdeps --notest --verbose .; then
+  cat /home/travis/.cpanm/build.log
+  exit 1
+fi
