8257db5675024a88c11784e986e5966cb69ebe4e
Antonio Terceiro <terceiro@softwarelivre.org>
2013-05-04 09:36:11 -0300

    Try travis build with a mix of native and perl dependencies

---
+++ travis.sh
@@ -0,0 +1,10 @@
+# doxyparse
+echo 'deb http://analizo.org/download/ ./' > /etc/apt/sources.list.d/analizo.list
+wget -O - http://analizo.org/download/signing-key.asc | apt-key add -
+apt-get update
+apt-get install doxyparse
+rm -rf /etc/apt/sources.list.d/analizo.list
+apt-get update
+
+# rest of non-perl deps - ruby, rake and cucumber assumed to be already installed
+apt-get install git sloccount sqlite3 man pkg-config uuid-dev libzmq-dev
