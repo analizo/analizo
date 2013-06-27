b4d3b0ef4bbd0c47555a68d54dff1e29336eee5d
KamillaaaH <holanda.kamilla@gmail.com>
2013-06-27 09:27:06 -0300

    Configuring architecture files

--- architecture/analizo-arch/analizo-arch.py
+++ architecture/analizo-arch/analizo-arch.py
@@ -14,18 +14,6 @@ app.config['BOOTSTRAP_FONTAWESOME'] = True
 app.config['SECRET_KEY'] = 'devkey'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6Lfol9cSAAAAADAkodaYl9wvQCwBMr3qGR_PPHcw'
 
-
-class ExampleForm(Form):
-    field1 = TextField('First Field', description='This is field one.')
-    field2 = TextField('Second Field', description='This is field two.',
-                       validators=[Required()])
-    hidden_field = HiddenField('You cannot see this', description='Nope')
-    recaptcha = RecaptchaField('A sample recaptcha field')
-
-    def validate_hidden_field(form, field):
-        raise ValidationError('Always wrong')
-
-
 @app.route('/')
 def index():
     return render_template('home.html')
