09af85af5240e44d21f16b7fc40036d4e4f4c131
Joenio Costa <joenio@colivre.coop.br>
2013-06-28 23:17:05 -0300

    Thas is no support in Test::BDD::Cucumber to execute Before{} and After{} blocks
    For now, I've just made a workaround to provide a way to execute code
    after every scenario...

---
+++ t/Analizo/Test/BDD/Cucumber/Harness.pm
@@ -0,0 +1,32 @@
+package t::Analizo::Test::BDD::Cucumber::Harness;
+use strict;
+use warnings;
+use Moose;
+ 
+extends 'Test::BDD::Cucumber::Harness::TermColor';
+
+use Cwd;
+our $top_dir = cwd();
+
+# Workaround to provide a way to run code after every scenario.
+# TODO: extend the Test::BDD::Cucumber to support defining code
+# to execute before and after every scenario and remove this class.
+#
+# Before {
+#   print "entering scenario";
+# };
+#
+# After {
+#   print "scenario done";
+# };
+
+sub scenario_done {
+  my ($self, $scenario, $dataset, $longest) = @_;
+  $self->SUPER::scenario_done($scenario, $dataset, $longest);
+  unlink 'tmp.out';
+  unlink 'tmp.err';
+  unlink glob('*.tmp');
+  chdir $top_dir;
+}
+
+1;
--- test.pl
+++ test.pl
@@ -9,8 +9,7 @@ print "I: Running acceptance tests with Test::BDD::Cucumber v$Test::BDD::Cucumbe
 use Test::BDD::Cucumber::Loader;
 
 # This harness prints out nice TAP
-use Test::BDD::Cucumber::Harness::TermColor;
-#use Test::BDD::Cucumber::Harness::TestBuilder;
+use t::Analizo::Test::BDD::Cucumber::Harness;
 
 # Load a directory with Cucumber files in it. It will recursively execute any
 # file matching .*_steps.pl as a Step file, and .*\.feature as a feature file.
@@ -19,8 +18,7 @@ use Test::BDD::Cucumber::Harness::TermColor;
 my ($executor, @features) = Test::BDD::Cucumber::Loader->load('t/features/');
 
 # Create a Harness to execute against. TestBuilder harness prints TAP
-my $harness = Test::BDD::Cucumber::Harness::TermColor->new({});
-#my $harness = Test::BDD::Cucumber::Harness::TestBuilder->new({});
+my $harness = t::Analizo::Test::BDD::Cucumber::Harness->new({});
 
 # For each feature found, execute it, using the Harness to print results
 $executor->execute($_, $harness) for @features;
