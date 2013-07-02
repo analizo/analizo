5187cba3dc634c125dcf079bbec5ebfee3a40e15
Joenio Costa <joenio@colivre.coop.br>
2013-07-02 10:22:01 -0300

    All tests pass

--- t/features/step_definitions/analizo_steps.pl
+++ t/features/step_definitions/analizo_steps.pl
@@ -3,7 +3,6 @@ use warnings;
 use Test::More 'no_plan';
 use Test::BDD::Cucumber::StepFile;
 use Method::Signatures;
-use Cwd;
 use File::Slurp;
 use File::Temp qw( tempdir );
 use File::Copy::Recursive qw( rcopy );
@@ -12,30 +11,15 @@ use feature "switch";
 use File::LibMagic;
 use Archive::Extract;
 
-our $top_dir = cwd();
-our $saved_path = $ENV{PATH};
-
-$ENV{LC_ALL} = 'C';
-$ENV{PATH} = "$top_dir:$ENV{PATH}";
-
-END {
-  chdir $top_dir;
-  $ENV{PATH} = $saved_path;
-};
-
 our $exit_status;
 our $stdout;
 our $stderr;
 
-sub run {
-  my ($command) = @_;
+When qr/^I run "([^\"]*)"$/, func($c) {
+  my $command = $1;
   $exit_status = system "($command) >tmp.out 2>tmp.err";
   $stdout = read_file('tmp.out');
   $stderr = read_file('tmp.err');
-}
-
-When qr/^I run "([^\"]*)"$/, func($c) {
-  run $1;
 };
 
 Then qr/^the output must match "([^\"]*)"$/, func($c) {
@@ -63,13 +47,12 @@ Step qr/^I copy (.*) into a temporary directory$/, func($c) {
 Given qr/^I create a file called (.+) with the following content$/, func($c) {
   open FILE, '>', $1 or die $!;
   if ($c->data =~ m/<\w+>/) {
-    # O Test::BDD::Cucumber nao substitui a string <key> pelo
-    # conteudo encontrado na tabela "Examples:", este codigo abaixo faz isto.
-    # TODO contribuir de volta com o projeto, criar testes, encontrar lugar
-    # no projeto Analizo para manter este trecho de código, ele não dev eficar
-    # aqui
-    # Ainda n sei como descobrir em qual linha do Exmaples estamos,
-    # portanto por hora crio entradas no arquivo com todas as linhas do Examples
+    # The Test::BDD::Cucumber not support replace strings <key> in the content
+    # by the values in table "Examples:", the code above does this.
+    # Not yet know how find out what line of "Examples:" we are, then for now
+    # we create entries for all values in the table.
+    # TODO Implement it on Test::BDD::Cucumber in the right way and contribute
+    # back to upstream.
     foreach my $row (@{ $c->scenario->data }) {
       foreach my $col (keys %$row) {
         (my $data = $c->data) =~ s/<$col>/$row->{$col}/sg;
