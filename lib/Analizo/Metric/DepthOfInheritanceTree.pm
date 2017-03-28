58ade1d25a69dc51ba55fa97b02f94c7720c0653
Izabela Cardoso <cizabelacristina@gmail.com>
2017-03-28 16:38:22 -0300

    Documenting depth of inheritance metric
    Signed-off-by: Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>

--- lib/Analizo/Metric/DepthOfInheritanceTree.pm
+++ lib/Analizo/Metric/DepthOfInheritanceTree.pm
@@ -2,6 +2,28 @@ package Analizo::Metric::DepthOfInheritanceTree;
 use strict;
 use base qw(Class::Accessor::Fast Analizo::ModuleMetric);
 
+=head1 NAME
+
+Analizo::Metric::DepthOfInheritanceTree - Depth Of Inheritance Tree metric
+
+=head1 DESCRIPTION
+
+The metric calculation is based on the following article and calculates the longest.
+path from a module to the class hierarchy root.
+
+Article: An empirical study of aspect-oriented metrics by Eduardo Kessler Piveta, 
+Ana Moreira, Marcelo Soares Pimenta, Joao Araujo, Pedro Guerreiro and R. Tom Price.
+
+See the paragraph about Depth Of Inheritance Tree in the article:
+
+"Considering a function s(x) : Module -> Module that computes the super-class or 
+super-aspect of a giver module, the value of DIT is given by:
+
+    DIT(m) = DIT(s(m)) + 1, ifc m != rootClass
+    DIT(m) = 0, otherwise."
+
+=cut
+
 __PACKAGE__->mk_accessors(qw( model ));
 
 sub new {
