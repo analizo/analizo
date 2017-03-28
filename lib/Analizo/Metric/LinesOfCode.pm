9af0d5ec55b9ccb6fb225dd0cc87a7003854ad82
Izabela Cardoso <cizabelacristina@gmail.com>
2017-03-28 16:56:42 -0300

    Documenting lines of code metric
    Signed-off-by: Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>

--- lib/Analizo/Metric/LinesOfCode.pm
+++ lib/Analizo/Metric/LinesOfCode.pm
@@ -3,6 +3,26 @@ use strict;
 use base qw(Class::Accessor::Fast Analizo::ModuleMetric);
 use Statistics::Descriptive;
 
+=head1 NAME
+
+Analizo::Metric::LinesOfCode - Lines Of Code (LOC) metric
+
+=head1 DESCRIPTION
+
+The metric calculation is based on the following article and calculates the program
+size in lines of code, excluding blank lines and comments.
+
+Article: The Lines of Code Metric as a Predictor of Program Faults: A 
+Critical Analysis by Taghi M. Khoshgoftaar and John C. Munson.
+
+See the paragraph about Lines Of COde in the article:
+
+"A description of the specific quantitative complexity data collected for 
+each program is as follows: 
+... program size in lines of code (non-comment, non-blank lines) in program (LOC)."
+
+=cut
+
 __PACKAGE__->mk_accessors(qw( model ));
 
 sub new {
