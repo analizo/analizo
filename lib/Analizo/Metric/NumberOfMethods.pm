1296a7fdca7862fd6352494072d5271ba6f1f730
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-03-30 17:49:45 -0300

    Documenting number of methods metric
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>
    Signed-off-by: Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>

--- lib/Analizo/Metric/NumberOfMethods.pm
+++ lib/Analizo/Metric/NumberOfMethods.pm
@@ -2,6 +2,27 @@ package Analizo::Metric::NumberOfMethods;
 use strict;
 use base qw(Class::Accessor::Fast Analizo::ModuleMetric);
 
+=head1 NAME
+
+Analizo::Metric::NumberOfMethods - Number Of Methods(NOM) metric
+
+=head1 DESCRIPTION
+
+The metric calculation is based on the following article and calculates the
+number of methods per class to measure the size of the classes in terms of its
+implemented operations.
+
+Article: Monitoring of source code metrics in open source projects by 
+Paulo Roberto Miranda Meirelles.
+
+See the adaptation of the paragraph about Number Of Methods in the article:
+
+"... This metric is used to help to identify the potential reuse of a class. In
+general, the classes with a large number of methods are harder to be reused,
+because they are more likely to be less cohesive (Lorenz and Kidd, 1994). Thus,
+it's recommended that a class don't have a excessive number of methods (Beck, 1997)."
+
+=cut
 
 __PACKAGE__->mk_accessors(qw( model ));
 
