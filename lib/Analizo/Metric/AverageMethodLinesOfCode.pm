c7b5a3d42f74745871302a5e5d26a3fc598dc369
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-03-30 17:20:55 -0300

    Documenting average method lines of code metric
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>
    Signed-off-by: Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>

--- lib/Analizo/Metric/AverageMethodLinesOfCode.pm
+++ lib/Analizo/Metric/AverageMethodLinesOfCode.pm
@@ -3,6 +3,27 @@ use strict;
 use base qw(Class::Accessor::Fast Analizo::ModuleMetric);
 use Statistics::Descriptive;
 
+=head1 NAME
+
+Analizo::Metric::AverageMethodLinesOfCode - Average Method Lines Of Code(AMLOC) metric
+
+=head1 DESCRIPTION
+
+The metric calculation is based on the following article and calculates the
+average number of lines of code per method.
+
+Article: Monitoring of source code metrics in open source projects by 
+Paulo Roberto Miranda Meirelles.
+
+See the adptation of the paragraph about Average Method Lines Of Code metric:
+
+"This metric indicates if the code is well distributed between the methods. How bigger,
+"heavier" are the methods. It's preferible to have a lot of small  and of easy
+understanding operations than few large and complex operations. The suggested intervals
+are: To 10 (good), between 10 and 13 (regular), from 13 onwards (bad)."
+
+=cut
+
 __PACKAGE__->mk_accessors(qw( model ));
 
 sub new {
