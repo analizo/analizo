97e0f98ba43257b5f44a6f6110ca3298b97cae7c
Joenio Costa <joenio@joenio.me>
2017-07-02 19:03:58 -0300

    using YAML::Syck instead of YAML::Tiny
    YAML::Tiny doesn't support keys with special chars, like "main(String [])"
    
       t/samples/hello_world/java/Main.java:
         Main:
           defines:
             - "main(String [])":
                 type: function
                 line: 2
                 protection: public

--- t/features/step_definitions/analizo_steps.pl
+++ t/features/step_definitions/analizo_steps.pl
@@ -6,7 +6,7 @@ use Method::Signatures;
 use File::Slurp;
 use File::Temp qw( tempdir );
 use File::Copy::Recursive qw( rcopy );
-use YAML::Tiny;
+use YAML::Syck;
 use feature "switch";
 use File::LibMagic;
 use Archive::Extract;
@@ -100,14 +100,14 @@ Then qr/^analizo must emit a warning matching "([^\"]*)"$/, func($c) {
 
 Then qr/^analizo must report that the project has (.+) = ([\d\.]+)$/, func($c) {
   my ($metric, $n) = ($1, $2);
-  my $stream = YAML::Tiny->read_string($stdout);
-  cmp_ok($stream->[0]->{$metric}, '==', $n);
+  my @stream = Load($stdout);
+  cmp_ok($stream[0]->{$metric}, '==', $n);
 };
 
 Then qr/^analizo must report that module (.+) has (.+) = (.+)$/, func($c) {
   my ($module, $metric, $value) = ($1, $2, $3);
-  my $stream = YAML::Tiny->read_string($stdout);
-  my ($module_metrics) = grep { $_->{_module} && $_->{_module} eq $module } @$stream;
+  my @stream = Load($stdout);
+  my ($module_metrics) = grep { $_->{_module} && $_->{_module} eq $module } @stream;
   for ($value) {
     when (/^\d+|\d+\.\d+$/) {
       cmp_ok($module_metrics->{$metric}, '==', $value);
@@ -121,18 +121,15 @@ Then qr/^analizo must report that module (.+) has (.+) = (.+)$/, func($c) {
 
 Then qr/^analizo must report that file (.+) not declares module (.+)$/, func($c) {
   my ($filename, $module) = ($1, $2);
-  my $stream = YAML::Tiny->read_string($stdout);
-  my ($document) = grep { $_->{_module} && $_->{_module} eq $module } @$stream;
+  my @stream = Load($stdout);
+  my ($document) = grep { $_->{_module} && $_->{_module} eq $module } @stream;
   ok(!grep { /^$filename$/ } @{$document->{_filename}});
 };
 
 Then qr/^analizo must report that file (.+) declares module (.+)$/, func($c) {
   my ($filename, $module) = ($1, $2);
-  my $stream = YAML::Tiny->read_string($stdout);
-  my ($document) = grep { $_->{_module} && $_->{_module} eq $module } @$stream;
-  #DEBUG
-  #my @filenames = @{$document->{_filename}};
-  #print "##### [@filenames] includes $filename ?\n";
+  my @stream = Load($stdout);
+  my ($document) = grep { $_->{_module} && $_->{_module} eq $module } @stream;
   ok(grep { /^$filename$/ } @{$document->{_filename}});
 };
 
