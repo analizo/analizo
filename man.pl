1a94d4aedd0618d24ba2db83531b415b0ceccda7
Joenio Costa <joenio@joenio.me>
2017-12-27 15:49:03 -0200

    moving out man.pl script

--- man.pl
+++
@@ -1,8 +0,0 @@
-our %manpages = ();
-for my $script (glob('lib/Analizo/Command/*')) {
-  my $path = 'blib/man1/analizo-' . basename($script, '.pm') . '.1';
-  $path =~ s/_/-/g;
-  $manpages{$script} = $path;
-}
-$manpages{'lib/Analizo.pm'} = 'blib/man1/analizo.1';
-
