be500502919443efc5c11363a8a2ac00f631e890
Joenio Costa <joenio@joenio.me>
2017-12-27 14:47:47 -0200

    moving the site sources to new repository
    new repository: https://github.com/analizo/site

--- site/site.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($) {
-  var host = document.location.host;
-  $('.repository').each(function() {
-    var html = $(this).html().replace(/analizo.org/g, host);
-    $(this).html(html);
-    });
-});
