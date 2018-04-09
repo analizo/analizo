1c6e202de5a32dea099d736b775df09a54582d8a
Joenio Costa <joenio@joenio.me>
2018-04-08 23:12:01 -0300

    documenting meaning for 0 (zero) value metrics
    issue #60
    
    thanks @rafamanzo

--- lib/Analizo/Metrics.pm
+++ lib/Analizo/Metrics.pm
@@ -6,6 +6,30 @@ use YAML;
 use Analizo::ModuleMetrics;
 use Analizo::GlobalMetrics;
 
+=head1 NAME
+
+Analizo::Metrics - reporting project-level and module-level metrics
+
+=head1 DESCRIPTION
+
+Analizo reports both project-level metrics, which are calculated for the entire
+project, and module-level metrics, which are calculated individually for each
+module. On the project-level, Analizo also provides basic descriptive
+statistics for each of the module-level metrics: sum, mean, median, mode,
+standard deviation, variance, skewness and kurtosis of the distribution,
+minimum, and maximum value.
+
+=head2 Null values
+
+As a project decision, Analizo consider metrics with 0 (zero) value as the same
+meaning of "null value", in other words, 0 (zero) has no meaning in the
+context of Analizo metrics calculation, both to project-level metrics as to
+module-level metrics. You can see a discussion on the
+L<issue #60|https://github.com/analizo/analizo/issues/60>
+about this decision.
+
+=cut
+
 __PACKAGE__->mk_accessors(qw(
     model
     module_metrics
@@ -96,4 +120,3 @@ sub metrics_for {
 }
 
 1;
-
