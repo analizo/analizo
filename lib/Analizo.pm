4a9bb320fed2c5a31cdf7d1edd679bdcc1887ecc
Joenio Marques da Costa <joenio@joenio.me>
2021-01-04 20:42:23 +0100

    update copyright year = 2021

--- lib/Analizo.pm
+++ lib/Analizo.pm
@@ -146,7 +146,7 @@ itself" mean either Artistic License or GPL version 1 or later.
 
 =item Copyright (c) 2008-2010 Antonio Terceiro
 
-=item Copyright (c) 2014-2019 Joenio Costa
+=item Copyright (c) 2014-2021 Joenio Costa
 
 =back
 
